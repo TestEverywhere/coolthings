e973618318991667f17fd471e6bf05362535d0ab
TestEveryWhere <rupalasanjay99@gmail.com>
2018-03-19 14:26:35 +0530

    First editing

---
+++ Test_sample1/src/Pack1/Test_auto.java
@@ -0,0 +1,13 @@
+package Pack1;
+
+public class Test_auto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Connect to Git"); 
+	
+	}
+	
+
+}
