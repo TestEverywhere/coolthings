d71bf83c673bf28db14a591926e0fbe5b6ec8d68
TestEveryWhere <rupalasanjay99@gmail.com>
2018-03-19 14:56:54 +0530

    Second

--- Test_sample1/src/Pack1/Test_auto.java
+++ Test_sample1/src/Pack1/Test_auto.java
@@ -1,14 +1,25 @@
 package Pack1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.junit.*;
 
 public class Test_auto {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	System.out.println("Connect to Git"); 
 	
-	//Git integration test for easy working 
+	
+	// TODO Auto-generated method stub
+
+		//System.out.println("Connect to Git"); 
+		
+		//Git integration test for easy working 
+		
+		
+		
+	@Test
+	public void junittest(){
+		
+		System.out.println("New lock");
+	
+	
 	}
-	
-
 }
